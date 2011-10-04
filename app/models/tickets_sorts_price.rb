bedc239e5957bc16cb2aa8e06709185ea32f37b3
PanPiotrek <piotrek89z.22>
2011-10-04 20:53:56 +0200

    dopisany model tickets_sorts_prices

--- app/models/tickets_sorts_price.rb
+++ app/models/tickets_sorts_price.rb
@@ -1,2 +1,16 @@
 class TicketsSortsPrice < ActiveRecord::Base
+  
+  belongs_to :discounts_sorts
+  belongs_to :cinemas
+  belongs_to :seamces_types
+  belongs_to :tickets_types
+  has_many :tickets
+  
+  validates_presence_of :cinema_id, :message => 'Pole cinema_id nie może być puste (Not NULL)'
+  validates_presence_of :seance_type_id, :message => 'Pole type_id nie może być puste (Not NULL)'
+  validates_presence_of :ticket_type_id, :message => 'Pole ticket_type_id nie może być puste (Not NULL)'
+  validates_presence_of :price, :message => 'Pole price nie może być puste (Not NULL)'
+  validates_presence_of :discount_sort_id, :message => 'Pole discount_sort_id nie może być puste (Not NULL)'
+  validates_presence_of :description, :message => 'Pole description nie może być puste (Not NULL)'
+  
 end

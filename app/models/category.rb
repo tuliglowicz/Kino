ef481f45805cff4b605f4853d398530338409584
PanPiotrek <piotrek89z.22>
2011-10-04 19:32:26 +0200

    poprawiony model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,5 +5,6 @@ class Category < ActiveRecord::Base
 	
 	default_scope order('name')
 	
-	validates_presence_of :name, :message => 'nie może być pusty (Not NULL)'
+	validates_presence_of :name, :message => ' Pole Category nie może być puste (Not NULL).'
+	validates_length_of :name, :maximum => 25
 end

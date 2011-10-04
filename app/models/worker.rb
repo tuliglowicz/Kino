70504c0e9d973101884b4515a58588af0ecd3d1b
PanPiotrek <piotrek89z.22>
2011-10-05 01:16:14 +0200

    worker update

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,15 +3,19 @@
 class Worker < ActiveRecord::Base	
 	belongs_to :status
 	belongs_to :cinema
+	has_many :tickets
 	
 	default_scope order('cinema_id, last_name, first_name')
 	
-	attr_accessor :password
 	
-	validates_presence_of :cinema_id, :status_id, :login, :password, :first_name, :last_name,  :message => 'nie może być pusty (Not NULL)'
+	validates_presence_of :cinema_id, :message => ' Pole cinema nie może być puste (Not NULL)'
+	validates_presence_of :status_id, :message => ' Pole status nie może być puste (Not NULL)'
+	validates_presence_of :login, :message => ' Pole login nie może być puste (Not NULL)'
+	validates_presence_of :first_name, :message => ' Pole first_name nie może być puste (Not NULL)' 
+	validates_presence_of :last_name, :message => ' Pole last_name nie może być puste (Not NULL)'
 	validates_numericality_of :cinema_id, :status_id, :phone, :only_integer => true, :message => 'musi być liczbą całkowitą!'
 	validates_uniqueness_of :login, :email, :allow_nil => true, :message => 'musi być unikalny; jest już w bazie'
-	validates_length_of :first_name, :last_name, :minimum => 3, :message => "musi być dłuższy niż 3 znaki"
+	validates_length_of :first_name, :last_name, :maximum => 30, :message => "musi być nie dłuższy niż 30 znaków"
 	validates_length_of :login, :password, :in => 3..12, :message => "musi być długości od 3 do 12"
 	validates_length_of :phone, :is => 9, :only_integer => true, :message => "musi byc 9-cio cyfrową liczbą całkowitą"
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => 'nieprawidłowy format'

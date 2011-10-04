82c3302f9513ebd7aa4dc1a8b7e6d481e1c55d78
PanPiotrek <piotrek89z.22>
2011-10-04 23:42:25 +0200

    poprawiony statuses

--- app/models/status.rb
+++ app/models/status.rb
@@ -4,5 +4,6 @@ class Status < ActiveRecord::Base
 	
 	default_scope order('name')
 	
-	validates_presence_of :name, :message => 'nie może być pusty (Not NULL)'
+	validates_presence_of :name, :message => 'Pole name nie może być puste (Not NULL)'
+	validates_length_of :name, :maximum => 25, :message => ' Pole name nie może mieć więcej niż 25 znaków '
 end

edb1ef2afe7708659936081632940f2c27c532dd
PanPiotrek <piotrek89z.22>
2011-10-05 18:50:22 +0200

    Poprawione filmy w widoku

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,11 @@
 class ReservationsController < ApplicationController
   # GET /reservations
   # GET /reservations.xml
+  
+  layout 'admin'
+  
+  before_filter :auth_exept_show, :except => ["show", "index", "edit", "update"]
+  
   def index
     @reservations = Reservation.all
 
@@ -81,4 +86,24 @@ class ReservationsController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+  private #===============================
+  def auth_exept_show
+    if session[:worker] == nil 
+        flash[:notice] = "Please log in, first!"
+        redirect_to(:controller => "public", :action => "index")
+        return false
+      else if session[:worker].status_id > 0
+        flash[:notice] = "Nie masz wymaganych uprawnień!"
+        if request.referer == "/"
+          redirect_to("/403.html")
+        else
+          redirect_to(request.referer)
+        end
+      end
+    end
+  end
+  
+  
+  
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -9,10 +9,10 @@ class Film < ActiveRecord::Base
 	validates_presence_of :title, :message => 'Pole title nie może być puste (Not NULL)' 
 	validates_presence_of :director, :message => 'Pole message nie może być puste (Not NULL)' 
 	validates_presence_of :year, :message => 'Pole year nie może być puste (Not NULL)'
-	validates_length_of :title, :maksimum => 100, :message => "Pole title musi być krótsze niż 100 znaków"
-	validates_length_of :director, :maksimum => 50, :message => "Pole director musi być krótsze niż 50 znaków"
-	validates_length_of :trailer, :maksimum => 75, :message => "Pole trailer musi być krótsze niż 75 znaków"
-	validates_length_of :poster, :maksimum => 50, :message => "Pole poster musi być krótsze niż 50 znaków"
+	validates_length_of :title, :maximum => 100, :message => 'Pole title musi być krótsze niż 100 znaków'
+	validates_length_of :director, :maximum => 50, :message => 'Pole director musi być krótsze niż 50 znaków'
+	validates_length_of :trailer, :maximum => 75, :message => 'Pole trailer musi być krótsze niż 75 znaków'
+	validates_length_of :poster, :maximum => 50, :message => 'Pole poster musi być krótsze niż 50 znaków'
 	validates_numericality_of :year, :only_integer => true, :greater_than => 1999, :less_than_or_equal_to => Time.now.year + 1, :message => "musi być liczbą między 1999, a " + (Time.now.year + 1).to_s
 	
 	

1c222e1c9e5bb7ac5c714039ae6d79e234cde8a5
PanPiotrek <piotrek89z.22>
2011-10-05 14:32:52 +0200

    poprawione seats

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -8,6 +8,7 @@ class Seat < ActiveRecord::Base
 
 	validates_presence_of :room_id, :message => ' Pole Room_id nie może być puste (Not NULL)'
 	validates_presence_of :row, :message => ' Pole row nie może być puste (Not NULL)'
+	validates_length_of :row, :maximum, :message => ' Pole row nie może być większe niż 2 znaki '
 	validates_format_of :row, :with => /[A-Z]/, :message => 'Pole row ma mieć dokładnie jeden znak A-Z'
 	validates_numericality_of :collumn, :room_id, :only_integer => true, :message => ' musi być liczbą całkowitą!'
 

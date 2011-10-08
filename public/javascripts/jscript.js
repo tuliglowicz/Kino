1b05a1429785f9ef70d21315223566a1b3d5aa4f
Włodek <w.tuliglowicz@gmail.com>
2011-10-08 14:07:32 +0200

    nowy layout, należy jeszcze poczyścić css-y i inne obrazki. Widzisz błąd w działaniu?- pisz do Włodka

--- public/javascripts/jscript.js
+++ public/javascripts/jscript.js
@@ -1,4 +1,8 @@
-﻿var xhr = false;
+﻿if (top.frames.length!=0) top.location=self.document.location;
+
+
+// -=========================================
+var xhr = false;
 var textRequest;
 var miasto;
 

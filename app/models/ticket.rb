905578f00cad9e56c652a9809214a3702fefe6df
PanPiotrek <piotrek89z.22>
2011-10-13 11:57:33 +0200

    update ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -9,6 +9,9 @@ class Ticket < ActiveRecord::Base
   #default_scope order('year DESC, title, category_id')
   
   validates_presence_of :seance_id, :message => 'Pole seance_id nie może być puste (Not NULL)' 
+  validates_presence_of :seat_id, :message => 'Pole seat_id nie może być puste (Not NULL)' 
+  validates_presence_of :user_id, :message => 'Pole user_id nie może być puste (Not NULL)' 
+  validates_presence_of :reservation_id, :message => 'Pole reservation_id nie może być puste (Not NULL)' 
   validates_presence_of :worker_id, :message => 'Pole worker_id nie może być puste (Not NULL)' 
   
 end

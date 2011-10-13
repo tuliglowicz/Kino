c1e0eb1b9efd09fdd3081c86bd0519c800b4412f
PanPiotrek <piotrek89z.22>
2011-10-13 11:50:19 +0200

    update

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,7 +6,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :worker
   
-  default_scope order('year DESC, title, category_id')
+  #default_scope order('year DESC, title, category_id')
   
   validates_presence_of :seance_id, :message => 'Pole seance_id nie może być puste (Not NULL)' 
   validates_presence_of :worker_id, :message => 'Pole worker_id nie może być puste (Not NULL)' 

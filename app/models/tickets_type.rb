0725144740464eb81fb976cad2d9a97223854294
PanPiotrek <piotrek89z.22>
2011-10-14 10:57:35 +0200

    destroy tickets_type

--- app/models/tickets_type.rb
+++
@@ -1,7 +0,0 @@
-# encoding: UTF-8
-class TicketsType < ActiveRecord::Base
-  
-  has_many :ticket_sort_prices
-  
-  validates_length_of :name, :maximum => 25, :message => ' Pole type nie może mieć więcej niż 25 znaków '
-end

de22c4238d7a3c6e99d8594dcfcd4023009346eb
PanPiotrek <piotrek89z.22>
2011-10-19 10:42:44 +0200

    update validations

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -6,10 +6,9 @@ class Seat < ActiveRecord::Base
 
 	default_scope order('room_id, row')
 
-	validates_presence_of :room_id, :message => ' Pole Sala nie może być puste.'
 	validates_presence_of :row, :message => ' Pole Rząd nie może być puste.'
 	validates_length_of :row, :maximum => 2, :message => ' Pole Rząd nie może zawierać więcej niż 2 znaki.'
 	validates_format_of :row, :with => /[A-Z]/, :message => 'Pole Rząd ma mieć dokładnie jeden znak A-Z.'
-	validates_numericality_of :collumn, :room_id, :only_integer => true, :message => 'Pole Kolumna musi być liczbą całkowitą!'
+	validates_numericality_of :collumn,  :only_integer => true, :message => 'Pole Kolumna musi być liczbą całkowitą!'
 
 end

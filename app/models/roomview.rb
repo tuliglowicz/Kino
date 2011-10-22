60731c68df37151c2b18b58d0d99219d1c05f59d
Włodek <w.tuliglowicz@gmail.com>
2011-10-22 17:34:18 +0200

    scaffold dla widoków

--- app/models/roomview.rb
+++ app/models/roomview.rb
@@ -2,4 +2,13 @@
 class Roomview < ActiveRecord::Base
 	has_many :rooms
 	
+	default_scope order('name')
+	
+	validates_presence_of :name, :message => 'Pole nazwy nie może być puste.'
+	validates_uniqueness_of :name, :message => "Nazwa jest już zajęta."	
+	validates_length_of :name, :minimum => 5, :message => 'Pole nazwy musi być dłuższe niż 5 znaków.'
+	validates_length_of :name, :maximum => 25, :message => 'Pole nazwy musi być krótsze niż 25 znaków.'
+	
+	validates_presence_of :view, :message => 'Musisz zbudować widok sali.'
+	#validates_uniqueness_of :view, :message => "Widok jest już w bazie."
 end

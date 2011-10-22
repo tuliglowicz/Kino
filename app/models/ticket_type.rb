aa78ca7b556b08ee4ed6b40c66539d9707148f56
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2011-10-22 22:49:31 +0200

    ticket_types - poprawki

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class TicketType < ActiveRecord::Base
   
-  has_many :ticket_sort_prices
+  has_many :ticket_sort_prices, :dependent => :delete_all
   
   validates_length_of :type, :maximum => 25, :message => ' Pole type nie może mieć więcej niż 25 znaków '
   validates_uniqueness_of :name, :message => 'Wprwadzono już ten rodzaj biletu'

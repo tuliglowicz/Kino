c0126aaa7b399c873bbca25802aed3c5a607dbe8
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2011-11-11 21:05:30 +0100

    wysylanie biletow

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -12,7 +12,7 @@ class Ticket < ActiveRecord::Base
   #default_scope order('year DESC, title, category_id')
   
   validates_presence_of :seance_id, :message => 'Pole Seans nie może być puste.' 
-  validates_presence_of :seat_id, :message => 'Pole Miejsce nie może być puste.' 
+  #validates_presence_of :seat_id, :message => 'Pole Miejsce nie może być puste.' 
   validates_presence_of :user_id, :message => 'Pole Użytkownik nie może być puste.' 
   validates_presence_of :reservation_id, :message => 'Pole Rezerwacja nie może być puste.' 
   validates_presence_of :worker_id, :message => 'Pole Pracownik nie może być puste.' 
